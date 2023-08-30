4067a8c4b6a97032af950c61b97a7dcf8bceaee7
Ligala Noah <ligalanoah@gmail.com>
2023-08-30 08:29:36 -0700

    corrections to -pow

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,14 +3,14 @@
 /**
  * factorial - function that calculates the factorial
  * of a givrn number
- * @n: integer for which the factorial is to be calculated 
+ * @n: integer for which the factorial is to be calculated
  * Return: integer value
  */
 int factorial(int n)
 {
-	if(n < 0)
+	if (n < 0)
 		return (-1);
 	if (n <= 1)
 		return (1);
-	return (n* factorial(n - 1));
+	return (n * factorial(n - 1));
 }
