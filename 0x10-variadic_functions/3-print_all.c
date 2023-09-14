2d3ff7659a39e2f4d1869405f577c1aca7271d68
Ligala Noah <ligalanoah@gmail.com>
2023-09-14 10:08:31 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,44 +8,43 @@
  */
 void print_all(const char * const format, ...)
 {
-	va_list ap;
-	const char *p;
-	char *sval;
-	int ival;
-	double dval;
-	char cval;
-	int comma = 0;
+	va_list args;
+	char *str;
+	int i = 0;
+	char current_format;
 
-	va_start(ap, format);
-	for (p = format; *p; ++p)
+	va_start(args, format);
+
+	while (format && format[i])
 	{
-		switch (*p)
+		current_format = format[i];
+		switch (current_format)
 		{
 			case 'c':
-				cval = va_arg(ap, int);
-				printf("%s%c", comma ? ", " : "", cval);
-				comma = 1;
+				printf("%c", va_arg(args, int));
 				break;
 			case 'i':
-				ival = va_arg(ap, int);
-				printf("%s%d", comma ? ", " : "", ival);
-				comma = 1;
+				printf("%d", va_arg(args, int));
 				break;
 			case 'f':
-				dval = va_arg(ap, double);
-				printf("%s%f", comma ? ", " : "", dval);
-				comma = 1;
+				printf("%f", (float)va_arg(args, double));
 				break;
 			case 's':
-				sval = va_arg(ap, char *);
-				if (sval == NULL)
-					printf("%s(nil)", comma ? ", " : "");
+				str = va_arg(args, char *);
+				if (str == NULL)
+					printf("(nil)");
 				else
-					printf("%s%s", comma ? ", " : "", sval);
-				comma = 1;
+					printf("%s", str);
 				break;
+			default:
+				i++;
+				continue;
 		}
+		if (format[i + 1])
+			printf(", ");
+		i++;
 	}
+
+	va_end(args);
 	printf("\n");
-	va_end(ap);
 }
