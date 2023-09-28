cae28c0ae3bb8963347475ae2fafb4573d3859b4
Ligala Noah <ligalanoah@gmail.com>
2023-09-28 11:17:51 -0700

    Update task

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the value of abit to a 0 at a given index
+ * @n: pointer to the unsigned long int
+ * @index: the index of the bit to clear (starting from 0)
+ *
+ * Return: 1 if successful otherwise -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(*n) * 8)
+		return (-1);
+
+	*n &= -(1UL << index);
+
+	return (1);
+}
