4f6591af1235b23b3797f83bbeaeef961497f20f
noahligala <63068195+noahligala@users.noreply.github.com>
2023-10-03 00:27:07 +0300

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,5 +11,9 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int openSourceFile(const char *filename);
+int openDestinationFile(const char *filename);
+int copyFile(int source_fd, int dest_fd);
+void closeFiles(int source_fd, int dest_fd);
 
 #endif
