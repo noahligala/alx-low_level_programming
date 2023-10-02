aa68185ab1e27d1b3225e32b059357eaac7c76e5
Ligala Noah <ligalanoah@gmail.com>
2023-10-02 15:59:04 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-#define BUFFER_SIZE 1024
-
 /**
  * main - Copy the content of one file to another.
  * @argc: The number of arguments.
@@ -10,53 +8,49 @@
  * Return: 0 on success, or an error code on failure.
  */
 
-int openSourceFile(const char *filename);
-int openDestinationFile(const char *filename);
-int copyFile(int source_fd, int dest_fd);
-void closeFiles(int source_fd, int dest_fd);
-
 int main(int argc, char *argv[])
 {
-    int source_fd, dest_fd;
+	int source_fd, dest_fd;
 
-    if (argc != 3)
-    {
-        dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
-        return (97);
-    }
-    
-    source_fd = openSourceFile(argv[1]);
-    dest_fd = openDestinationFile(argv[2]);
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
+		return (97);
+	}
 
-    if (source_fd == -1 || dest_fd == -1)
-        return (98);
+	source_fd = openSourceFile(argv[1]);
+	dest_fd = openDestinationFile(argv[2]);
 
-    if (copyFile(source_fd, dest_fd) == -1)
-    {
-        closeFiles(source_fd, dest_fd);
-        return (99);
-    }
-    
-    closeFiles(source_fd, dest_fd);
-    return (0);
+	if (source_fd == -1 || dest_fd == -1)
+		return (98);
+
+	if (copyFile(source_fd, dest_fd) == -1)
+	{
+		closeFiles(source_fd, dest_fd);
+		return (99);
+	}
+
+	closeFiles(source_fd, dest_fd);
+	return (0);
 }
 
 /**
  * openSourceFile - Opens a source file for reading.
- * @filename: The name of the source file to open.
+ * @filename: the name of the source file
  *
  * Return: The file descriptor of the opened file, or -1 on failure.
  */
 
 int openSourceFile(const char *filename)
 {
-    int fd = open(filename, O_RDONLY);
-    if (fd == -1)
-    {
-        dprintf(2, "Error: Can't read from file %s\n", filename);
-        return (-1);
-    }
-    return (fd);
+	int fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", filename);
+		return (-1);
+	}
+	return (fd);
 }
 
 /**
@@ -68,13 +62,16 @@ int openSourceFile(const char *filename)
 
 int openDestinationFile(const char *filename)
 {
-    int fd_dest = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-    if (fd_dest == -1)
-    {
-        dprintf(2, "Error: Can't write to %s\n", filename);
-        return (-1);
-    }
-    return (fd_dest);
+	int fd_dest = open(filename,
+			O_WRONLY | O_CREAT | O_TRUNC,
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+
+	if (fd_dest == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", filename);
+		return (-1);
+	}
+	return (fd_dest);
 }
 
 /**
@@ -86,26 +83,25 @@ int openDestinationFile(const char *filename)
  */
 int copyFile(int source_fd, int dest_fd)
 {
-    char buffer[BUFFER_SIZE];
-    ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+	ssize_t bytes_read, bytes_written;
 
-    while ((bytes_read = read(source_fd, buffer, BUFFER_SIZE)) > 0)
-    {
-        bytes_written = write(dest_fd, buffer, bytes_read);
-        if (bytes_written == -1 || bytes_written != bytes_read)
-        {
-            dprintf(2, "Error: Can't write to destination file\n");
-            return (-1);
-        }
-    }    
-    
-    if (bytes_read == -1)
-    {
-        dprintf(2, "Error: Can't read from source file\n");
-        return (-1);
-    }
+	while ((bytes_read = read(source_fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(dest_fd, buffer, bytes_read);
+		if (bytes_written == -1 || bytes_written != bytes_read)
+		{
+			dprintf(2, "Error: Can't write to destination file\n");
+			return (-1);
+		}
+	}
+	if (bytes_read == -1)
+	{
+		dprintf(2, "Error: Can't read from source file\n");
+		return (-1);
+	}
 
-    return (0);
+	return (0);
 }
 
 /**
@@ -114,13 +110,13 @@ int copyFile(int source_fd, int dest_fd)
  * @dest_fd: The file descriptor of the destination file.
  *
  * This function closes the specified file descriptors.
- * If an error occurs while closing a file descriptor, it prints an error message.
+ * If an error occurs while closing a file descriptor, it prints an error msg.
  */
 void closeFiles(int source_fd, int dest_fd)
 {
-    if (close(source_fd) == -1 || close(dest_fd) == -1)
-    {
-        dprintf(2, "Error: Can't close file descriptor\n");
-        exit(100);
-    }
+	if (close(source_fd) == -1 || close(dest_fd) == -1)
+	{
+		dprintf(2, "Error: Can't close file descriptor\n");
+		exit(100);
+	}
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,13 +7,16 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#define BUFFER_SIZE 1024
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
 int openSourceFile(const char *filename);
 int openDestinationFile(const char *filename);
-int copyFile(int source_fd, int dest_fd);
+int copyFile(int sourc_fd, int dest_id);
 void closeFiles(int source_fd, int dest_fd);
 
 #endif
