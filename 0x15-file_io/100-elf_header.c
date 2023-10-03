bfa908ff41b0397392b3751ce32f2ced07b7d6cb
noahligala <63068195+noahligala@users.noreply.github.com>
2023-10-03 03:05:27 +0300

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,87 +1,81 @@
 #include "main.h"
 
-void displayElfHeader(const Elf64_Ehdr *elfHeader)
-{
-	int i;
+void displayElfHeader(const Elf64_Ehdr *elfHeader) {
+    printf("ELF Header:\n");
+    printf("  Magic:   ");
+    for (int i = 0; i < 16; i++) {
+        printf("%02x ", elfHeader->e_ident[i]);
+    }
+    printf("\n");
+    printf("  Class:                             ELF%d\n", elfHeader->e_ident[4] == 1 ? 32 : 64);
+    printf("  Data:                              2's complement, %s endian\n", elfHeader->e_ident[5] == 1 ? "little" : "big");
+    printf("  Version:                           %d (current)\n", elfHeader->e_version);
+    printf("  OS/ABI:                            UNIX - ");
+    switch (elfHeader->e_ident[7]) {
+        case 0:
+            printf("System V\n");
+            break;
+        case 3:
+            printf("Linux\n");
+            break;
+        case 6:
+            printf("Solaris\n");
+            break;
+        case 53:
+            printf("NetBSD\n");
+            break;
+        default:
+            printf("<unknown: %d>\n", elfHeader->e_ident[7]);
+    }
+    printf("  ABI Version:                       %d\n", elfHeader->e_ident[8]);
+    printf("  Type:                              ");
+    switch (elfHeader->e_type) {
+        case 1:
+            printf("REL (Relocatable file)\n");
+            break;
+        case 2:
+            printf("EXEC (Executable file)\n");
+            break;
+        case 3:
+            printf("DYN (Shared object file)\n");
+            break;
+        case 4:
+            printf("CORE (Core file)\n");
+            break;
+        default:
+            printf("<unknown: %d>\n", elfHeader->e_type);
+    }
+    printf("  Entry point address:               0x%lx\n", elfHeader->e_entry);
+}
 
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        dprintf(2, "Usage: %s elf_filename\n", argv[0]);
+        return 98;
+    }
 
-	printf("ELF Header:\n");
-	printf("	Magic:	");
-	for (i = 0; i < 16; i++)
-	{
-		printf("%02x ", elfHeader->e_ident[i]);
-	}
+    const char *elfFilename = argv[1];
+    int fd = open(elfFilename, O_RDONLY);
 
-	printf("\n");
-	printf(" Class:			ELF%d\n", elfHeader->e_ident[4] == 1 ? 32 : 64);
-	printf(" Data:			2's compliment, %s endian\n", elfHeader->e_ident[5] == 1 ? "little" : "big");
-	printf(" Version:		%d (current)\n", elfHeader->e_version);
-	printf(" OS/ABI:		UNIX - ");
-	switch (elfHeader->e_ident[7])
-	{
-		case 0:
-			printf("System V\n");
-			break;
+    if (fd == -1) {
+        dprintf(2, "Error: Can't open file %s\n", elfFilename);
+        return 98;
+    }
 
-		case 3:
-			printf("Linux\n");
-			break;
-		case 6:
-			printf("Solaris\n");
-			break;
-		case 53:
-			printf("NetBSD\n");
-			break;
-		default:
-			printf("<unknown: %d>\n", elfHeader->e_ident[7]);
-	}
-	print(" ABI Version:		%d\n", elfHeader->e_ident[8]);
-	printf(" Type:			");
-	switch (elfHeader->e_type)
-	{
-		case 1:
-			printf("REL (Relocatable file)\n");
-			break;
-		case 2:
-			printf("EXEC (Executable file)\n");
-			break;
-		case 3:
-			printf("DYN (Shared object file)\n");
-			break;
-		case 4:
-			printf("CORE (Core file)\n");
-			break;
-		default:
-			printf("<unknown: %d>\n", elfHeader->e_type);
-	}
+    Elf64_Ehdr elfHeader;
+    ssize_t bytesRead = read(fd, &elfHeader, sizeof(elfHeader));
 
-	int main(int argc, char *argv[])
-	{
-		if (argc != 2)
-		{
-			dprint(2, "Usage: %s elf_filename\n", argv[0]);
-			return (98);
-		}
+    if (bytesRead != sizeof(elfHeader)) {
+        dprintf(2, "Error: Can't read ELF header from file %s\n", elfFilename);
+        close(fd);
+        return 98;
+    }
 
+    displayElfHeader(&elfHeader);
+    close(fd);
 
-		const char *elfFilename = argv[1];
-		int fd = open(elfFilename, O_RDONLY);
-
-		if (fd == -1)
-		{
-			dprintf(2, "Error: Can't open file %s\n", elfFilename);
-			return (98);
-		}
-
-		Elf64_Ehdr eldHeader;
-		ssize_t bytesRead = read(fd, elfHeader, sizeof(elfHeader));
-
-		if (bytesRead != sizeof(elfHeader))
-		{
-			dprintf(2, "Error: Can't Read ELF header from file %s\n", elfFilename);
-			close(fd);
-			return (98);
-		}
+    return 0;
+}
 
 		displayElfHeader(&elfHeader);
 		close(fd);
